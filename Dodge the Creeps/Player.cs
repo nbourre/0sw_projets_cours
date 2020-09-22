9c053097e7f22d99ff3a9ef8eff5dc8ec0579d57
Nicolas Bourré <nbourrereg@hotmail.com>
2020-09-22 19:59:31 -0400

    Player and mobs are working

--- Dodge the Creeps/Player.cs
+++ Dodge the Creeps/Player.cs
@@ -3,9 +3,10 @@ using System;
 
 public class Player : Area2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+
+    [Signal]
+    public delegate void Hit();
+
     [Export]
     public int Speed = 400;
 
@@ -15,6 +16,7 @@ public class Player : Area2D
     public override void _Ready()
     {
         _screenSize = GetViewport().Size;
+        Hide();
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -44,11 +46,41 @@ public class Player : Area2D
 
         var animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
 
-        if (velocity.Lenght() > 0) {
+        if (velocity.Length() > 0) {
             velocity = velocity.Normalized() * Speed;
             animatedSprite.Play();
         } else {
             animatedSprite.Stop();
         }
+
+        Position += velocity * delta;
+        Position = new Vector2(
+          x: Mathf.Clamp(Position.x, 0, _screenSize.x),
+          y: Mathf.Clamp(Position.y, 0, _screenSize.y)  
+        );
+
+        if (velocity.x != 0) {
+            animatedSprite.Animation = "walk";
+            animatedSprite.FlipV = false;
+
+            animatedSprite.FlipH = velocity.x < 0;
+        } else if (velocity.y != 0) {
+            animatedSprite.Animation = "up";
+            animatedSprite.FlipV = velocity.y > 0;
+        }
+    }
+
+    public void OnPlayerBodyEntered(PhysicsBody2D body)
+    {
+        Hide(); // Player disappears after being hit.
+        EmitSignal("Hit");
+        GetNode<CollisionShape2D>("CollisionShape2D").SetDeferred("disabled", true);
+    }
+
+    public void Start(Vector2 pos)
+    {
+        Position = pos;
+        Show();
+        GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
     }
 }
