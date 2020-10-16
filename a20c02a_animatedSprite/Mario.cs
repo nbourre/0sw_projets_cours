37c48b497b121fc559baba4716dd5d2ec2bd959a
Nicolas Bourré <nbourrereg@hotmail.com>
2020-10-16 13:29:43 -0400

    End of animatedSprite part

--- a20c02a_animatedSprite/Mario.cs
+++ a20c02a_animatedSprite/Mario.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public class KinematicBody2D : Godot.KinematicBody2D
+public class Mario : Godot.KinematicBody2D
 {
     // Declare member variables here. Examples:
     // private int a = 2;
@@ -16,18 +16,19 @@ public class KinematicBody2D : Godot.KinematicBody2D
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
+        // Récupération du noeud enfant nommé AnimatedSprite
         var currentSprite = (AnimatedSprite)GetNode("AnimatedSprite");
-
+        
+        // Gestion des entrées
         if (Input.IsActionPressed("ui_right")) {
             currentSprite.FlipH = false;
             currentSprite.Play("run");
-
         } else if (Input.IsActionPressed("ui_left")) {
             currentSprite.FlipH = true;
             currentSprite.Play("run");
         } else {
             currentSprite.Play("stop");
         }
-
     }
+
 }
---
+++ a20c02a_animatedSprite/Node2D.cs
@@ -0,0 +1,36 @@
+using Godot;
+using System;
+
+public class Node2D : Godot.Node2D
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        
+
+       
+    }
+
+    public override void _UnhandledKeyInput(InputEventKey @event)
+    {
+        base._UnhandledKeyInput(@event);
+
+        if (@event is InputEventKey eventKey) {
+            if (eventKey.Pressed) {
+                var lbl_Control = (Label)GetNode("lbl_Control");
+                
+                lbl_Control.Text = $"Key : {Enum.GetName(typeof(KeyList), eventKey.Scancode)}";
+            }
+        }
+    }
+}
