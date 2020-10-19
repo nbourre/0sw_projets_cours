c5864d7ea0b4f96b53dc7c5fe228817cb8da92e2
Nicolas Bourré <nbourrereg@hotmail.com>
2020-10-19 15:49:27 -0400

    Simple movement ok

--- a20c02b_playerMovement/Player.cs
+++ a20c02b_playerMovement/Player.cs
@@ -15,30 +15,30 @@ public class Player : KinematicBody2D
         
     }
 
-    public Vector2 GetInput()
-    {
-        var input_vector = Vector2.Zero;
-        input_vector.x = Input.GetActionStrength("ui_right") 
-                            - Input.GetActionStrength("ui_left");
-        input_vector.y = Input.GetActionStrength("ui_down") 
-                            - Input.GetActionStrength("ui_up");
-        
-        input_vector = input_vector.Normalized();
+        public Vector2 GetInput()
+        {
+            var input_vector = Vector2.Zero;
+            input_vector.x = Input.GetActionStrength("ui_right") 
+                                - Input.GetActionStrength("ui_left");
+            input_vector.y = Input.GetActionStrength("ui_down") 
+                                - Input.GetActionStrength("ui_up");
+            
+            input_vector = input_vector.Normalized();
 
-        return input_vector;
-    }
-
-    public override void _PhysicsProcess(float delta)
-    {
-        var input_vector = GetInput();
-
-        if (input_vector != Vector2.Zero) {
-            Velocity = input_vector * MAX_SPEED;
-        } else {
-            Velocity = Vector2.Zero;
+            return input_vector;
         }
 
-        MoveAndCollide(Velocity * delta);
-    }
+        public override void _PhysicsProcess(float delta)
+        {
+            var input_vector = GetInput();
+
+            if (input_vector != Vector2.Zero) {
+                Velocity = input_vector * MAX_SPEED;
+            } else {
+                Velocity = Vector2.Zero;
+            }
+
+            MoveAndCollide(Velocity * delta);
+        }
 
 }
