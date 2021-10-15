117c0073cf2b8228123b15b3d0fa98a100e0bf7a
Nicolas Bourré <nbourrereg@hotmail.com>
2021-10-14 21:09:56 -0400

    Added camera movement with limits and player

---
+++ c04a_tileset/player.cs
@@ -0,0 +1,46 @@
+using Godot;
+using System;
+
+public class player : KinematicBody2D
+{
+    const int MAX_SPEED = 200;
+    const int ACCELERATION = 25;
+    const int FRICTION = 25;
+
+    Vector2 input_vector = new Vector2();
+    public Vector2 Velocity = Vector2.Zero;
+
+    
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+    private Vector2 getInput() {
+        input_vector *= 0; // Réinitialisation
+
+        input_vector.x = Input.GetActionStrength("ui_right") 
+                            - Input.GetActionStrength("ui_left");
+        input_vector.y = Input.GetActionStrength("ui_down") 
+                            - Input.GetActionStrength("ui_up");
+        
+        input_vector = input_vector.Normalized();
+        return input_vector;
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _PhysicsProcess(float delta)
+    {
+        input_vector = getInput();
+
+        if (input_vector != Vector2.Zero) {
+            Velocity = Velocity.MoveToward(input_vector * MAX_SPEED, ACCELERATION);
+        } else {
+            Velocity = Velocity.MoveToward(Vector2.Zero, FRICTION);
+        }
+
+        Velocity = MoveAndSlide(Velocity);
+    }
+}
---
+++ c04a_tileset/world.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public class world : Node2D
+{
+    Camera2D camera2D;
+    int tileSize = 64;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        camera2D = GetNode<Camera2D>("player/Camera2D");
+
+        camera2D.LimitLeft = 0 + tileSize;
+        camera2D.LimitTop = 0 + tileSize;
+        camera2D.LimitRight = 1900 - tileSize;
+        camera2D.LimitBottom = 650 - tileSize;
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
