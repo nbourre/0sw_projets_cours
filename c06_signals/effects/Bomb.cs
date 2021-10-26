3d7f16e5d451438b43e8a6a5c39de3b7a7e7d4eb
Nicolas Bourré <nbourrereg@hotmail.com>
2021-10-25 20:22:50 -0400

    New project to show signals

---
+++ c06_signals/effects/Bomb.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+
+public class Bomb : Node2D
+{
+    [Signal]
+    public delegate void OnExplode(Vector2 position);
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+    public void onArea2DInputEvent(Viewport viewport, InputEvent ev, int shape_idx) {
+
+        var btn = ev as InputEventMouseButton;
+
+        if (btn != null && btn.ButtonIndex == (int)ButtonList.Left && ev.IsPressed()) {
+            GD.Print("Clicked!");
+            Explode();
+        }
+    }
+
+    public void Explode() {
+        EmitSignal(nameof(OnExplode), Position);
+        this.QueueFree();
+    }
+}
