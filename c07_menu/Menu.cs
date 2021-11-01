3bc82b5e61f2bf30002305711a2e1ad5d471aa8d
nbourre <nbourrereg@hotmail.com>
2021-11-01 12:48:29 -0400

    Ajout du projet c07_menu qui crée un menu simple

---
+++ c07_menu/Menu.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public class Menu : Control
+{
+    
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        GetNode<Button>("VBoxContainer/StartButton").GrabFocus();        
+    }
+
+    public void onStartButtonPressed() {
+        GetTree().ChangeScene("res://sceneTest.tscn");
+    }
+
+    public void onOptionsButtonPressed() {
+        var options = GD.Load<PackedScene>("res://optionsMenu.tscn").Instance();
+        GetTree().CurrentScene.AddChild(options);
+    }
+
+    public void onQuitButtonPressed() {
+        GetTree().Quit();
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
