57432978d174ffdf3d97372de1af8e159f6b1463
Nick B (macOS) <nbourrereg@hotmail.com>
2022-11-21 12:19:31 -0500

    First throw for the Minesweeper class

---
+++ wip/minesweeper_logic/Minesweeper.cs
@@ -0,0 +1,147 @@
+class Minesweeper
+{
+    List<List<Cell>> board;
+    Random random = new Random();
+
+    public int Width { get; set; } = 10;
+    public int Height { get; set; } = 10;
+    public double Density { get; set; } = 0.15;
+    public class Cell
+    {
+        public bool IsMine { get; set; } = false;
+        public bool IsRevealed { get; set; } = false;
+        public int AdjacentMines { get; set; }
+
+        public override string ToString()
+        {
+            if (IsRevealed)
+            {
+                if (IsMine)
+                {
+                    return "X";
+                }
+                else
+                {
+                    return AdjacentMines.ToString();
+                }
+            }
+            else
+            {
+                return " ";
+            }
+        }
+    }
+
+    public Minesweeper(int sizeX, int sizeY, double density = 0.15)
+    {
+        Width = sizeX;
+        Height = sizeY;
+        Density = density;
+        GenerateBoard();
+    }
+
+    /// <summary>
+    /// Function that generate the minesweeper board
+    /// </summary>
+    public void GenerateBoard()
+    {
+        int nbCells = Width * Height;
+
+        if (board == null)
+        {
+            board = new List<List<Cell>>();
+            for (int j = 0; j < Height; j++)
+            {
+                board.Add(new List<Cell>());
+                for (int i = 0; i < Width; i++)
+                {
+                    var cell = new Cell();
+
+                    // Randomly generate mines
+                    if (random.Next(0, 100) < (Density * 100))
+                    {
+                        cell.IsMine = true;
+                    }
+                    board[j].Add(cell);
+                }
+            }
+        }
+
+        // Calculate the number of adjacent mines
+        GenerateNumbers();
+    }
+
+    /// <summary>
+    /// Generate numbers for each cell
+    /// </summary>
+    private void GenerateNumbers() 
+    {
+        for (int j = 0; j < sizeY; j++)
+        {
+            for (int i = 0; i < sizeX; i++)
+            {
+                if (!board[j][i].IsMine)
+                {
+                    board[j][i].AdjacentMines = GetAdjacentMines(i, j);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Get the number of adjacent mines for a given cell
+    /// </summary>
+    /// <param name="x">X position of the cell</param>
+    /// <param name="y">Y position of the cell</param>
+    /// <returns>Number of adjacent mines</returns>
+    /// <remarks>Return -1 if the cell is a mine</remarks>
+    /// <exception cref="ArgumentOutOfRangeException">If the cell is out of the board</exception>
+    /// <exception cref="NullReferenceException">If the board is not generated</exception>
+    /// <exception cref="InvalidOperationException">If the board is not generated</exception>
+    /// <exception cref="IndexOutOfRangeException">If the cell is out of the board</exception>
+    private int GetAdjacentMines(int x, int y)
+    {
+        if (board == null)
+        {
+            throw new InvalidOperationException("The board is not generated");
+        }
+
+        if (x < 0 || x >= sizeX || y < 0 || y >= sizeY)
+        {
+            throw new ArgumentOutOfRangeException("The cell is out of the board");
+        }
+
+        int count = 0;
+
+        for (int j = y - 1; j <= y + 1; j++)
+        {
+            for (int i = x - 1; i <= x + 1; i++)
+            {
+                if (i >= 0 && i < sizeX && j >= 0 && j < sizeY)
+                {
+                    if (board[j][i].IsMine)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+
+    override public string ToString()
+    {
+        string result = "";
+        for (int j = 0; j < sizeY; j++)
+        {
+            for (int i = 0; i < sizeX; i++)
+            {
+                result += board[j][i].ToString();
+            }
+            result += "\n";
+        }
+        return result;
+    }
+    
+}
