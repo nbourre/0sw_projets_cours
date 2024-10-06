508192a3f53e4b4bc0ae7b97518dc366eb37d8f8
Nick B <nbourrereg@hotmail.com>
2024-10-06 16:36:15 -0400

    Ajout du projet Finite State Machine

---
+++ c08_fsm_done/generic_character.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+class_name GenericCharacter
+
+var anim_player : AnimationPlayer
+var sprite : Sprite2D
+var direction : Vector2
+
+func get_animation_player () -> AnimationPlayer:
+	return anim_player
