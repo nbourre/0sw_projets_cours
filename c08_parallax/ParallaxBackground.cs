ab0b8ad7fa36278a30d4b8b2c3cf026f6f65bc92
Nick B (macOS) <nbourrereg@hotmail.com>
2024-10-07 13:38:58 -0400

    Modif du mac

--- c08_parallax/ParallaxBackground.cs
+++ c08_parallax/ParallaxBackground.cs
@@ -3,24 +3,24 @@ using System;
 
 public partial class ParallaxBackground : Godot.ParallaxBackground
 {
-    [Export]
-    float Cloud_Speed = -10f;
+	[Export]
+	float Cloud_Speed = -10f;
 
-    ParallaxLayer cloudLayer;
-    Sprite2D cloudSprite;
+	ParallaxLayer cloudLayer;
+	Sprite2D cloudSprite;
 
-    public override void _Ready()
-    {        
-        cloudLayer = GetNode<ParallaxLayer>("clouds");
-        cloudSprite = cloudLayer.GetNode<Sprite2D>("Sprite2D");
-    }
+	public override void _Ready()
+	{        
+		cloudLayer = GetNode<ParallaxLayer>("clouds");
+		cloudSprite = cloudLayer.GetNode<Sprite2D>("Sprite2D");
+	}
 
-    public override void _Process(double delta)
-    {
-        // Move the cloud automatically
-        cloudLayer.SetMotionOffset(
-            new Vector2(
-                cloudLayer.GetMotionOffset().X + (Cloud_Speed * (float)delta),
-                0));      
-    }
+	public override void _Process(double delta)
+	{
+		// Move the cloud automatically
+		cloudLayer.SetMotionOffset(
+			new Vector2(
+				cloudLayer.GetMotionOffset().X + (Cloud_Speed * (float)delta),
+				0));      
+	}
 }
