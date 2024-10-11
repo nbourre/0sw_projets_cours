240f475a1871639abc7be9c8be7de47638f01f1d
Nick B (macOS) <nbourrereg@hotmail.com>
2024-10-11 10:27:56 -0400

    Opened on mac

---
+++ wip/i'm-cool-and-i-fly/states/player_forward.gd
@@ -0,0 +1,24 @@
+extends BaseState
+class_name PlayerForward
+
+@export var player : GenericCharacter
+var anim_player : AnimationPlayer
+
+func manage_input() -> void:	
+	var dir : Vector2 = Input.get_vector("left", "right", "up", "down").normalized()
+	
+	if (dir.length() == 0):
+		Transitioned.emit(self, "idle")		
+
+func enter():
+	anim_player = player.get_animation_player()	
+	
+func update(delta: float) -> void:
+	if not anim_player :
+		anim_player = player.get_animation_player()
+	manage_input()
+	
+func physics_update(delta: float) -> void:
+	if not anim_player : return
+		
+	anim_player.play("forward")
