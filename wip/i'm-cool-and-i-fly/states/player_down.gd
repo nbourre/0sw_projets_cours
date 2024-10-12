87d471f597363f93c401d0a611a2dd4590c89995
Nick B <nbourrereg@hotmail.com>
2024-10-11 21:35:00 -0400

    Player states done

--- wip/i'm-cool-and-i-fly/states/player_down.gd
+++ wip/i'm-cool-and-i-fly/states/player_down.gd
@@ -1,5 +1,5 @@
 extends BaseState
-class_name PlayerIdle
+class_name PlayerDown
 
 @export var player : GenericCharacter
 var anim_player : AnimationPlayer
@@ -7,14 +7,16 @@ var anim_player : AnimationPlayer
 func manage_input() -> void:	
 	var dir : Vector2 = Input.get_vector("left", "right", "up", "down").normalized()
 	
+	if (dir.length() == 0):
+		Transitioned.emit(self, "idle")		
+		
 	if (dir.x > 0):
 		Transitioned.emit(self, "forward")
 	if (dir.x < 0):
 		Transitioned.emit(self, "backward")
 	if (dir.y < 0):
 		Transitioned.emit(self, "up")
-	if (dir.y > 0):
-		Transitioned.emit(self, "down")
+
 
 
 func enter():
@@ -23,9 +25,10 @@ func enter():
 func update(delta: float) -> void:
 	if not anim_player :
 		anim_player = player.get_animation_player()
+		
 	manage_input()
 	
 func physics_update(delta: float) -> void:
 	if not anim_player : return
 		
-	anim_player.play("idle")
+	anim_player.play("down")
