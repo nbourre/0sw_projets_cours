2b462c54ca9d98b95c239c158bfaf6dafe010fee
Nick B <nbourrerg@hotmail.com>
2024-10-28 15:21:36 -0400

    wip : Texture Progress Bar

---
+++ c10b_ui_TextureProgressBar/world.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@onready var player : Player = $Player
+@onready var level : LevelOne = $Level
+
+func is_on_arcade() -> bool:
+	return OS.get_executable_path().to_lower().contains("retropie")
+
+func manage_end_game() -> void:
+	if is_on_arcade() :
+		if Input.is_action_pressed("hotkey") and Input.is_action_pressed("quit"):
+			get_tree().quit()
+	else :
+		if Input.is_action_just_pressed("quit"):
+			get_tree().quit()
+
+
+func manage_limits() -> void:
+	player.position.y = clamp(player.position.y, level.top, level.bottom)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	manage_end_game()
+	manage_limits()
