589bacfc4cc24daee8f1ff642795b5e924ab6120
Nick B <nbourrereg@hotmail.com>
2024-10-29 20:20:45 -0400

    exemple TextureProgressBar fonctionne

--- c10b_ui_TextureProgressBar/world.gd
+++ c10b_ui_TextureProgressBar/world.gd
@@ -1,8 +1,10 @@
+class_name World
 extends Node2D
 
 @onready var player : Player = $Player
 @onready var level : LevelOne = $Level
 
+#region Arcade stuff
 func is_on_arcade() -> bool:
 	return OS.get_executable_path().to_lower().contains("retropie")
 
@@ -13,16 +15,18 @@ func manage_end_game() -> void:
 	else :
 		if Input.is_action_just_pressed("quit"):
 			get_tree().quit()
-
+#endregion
 
 func manage_limits() -> void:
 	player.position.y = clamp(player.position.y, level.top, level.bottom)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	manage_end_game()
 	manage_limits()
+	
