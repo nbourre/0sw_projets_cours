2e884c68397e7bef82ad6d250b841442ded7c0ee
Nick B <nbourrereg@hotmail.com>
2024-10-29 20:40:56 -0400

    ProgressBar done

--- wip/i'm-cool-and-i-fly/scenes/world.gd
+++ wip/i'm-cool-and-i-fly/scenes/world.gd
@@ -3,6 +3,9 @@ extends Node2D
 @onready var player : Player = $Player
 @onready var level : LevelOne = $Level
 
+@onready var hp_bar : GenericProgressBar = $CanvasLayer/GenericProgressBar
+
+#region Arcade stuff
 func is_on_arcade() -> bool:
 	return OS.get_executable_path().to_lower().contains("retropie")
 
@@ -14,12 +17,16 @@ func manage_end_game() -> void:
 		if Input.is_action_just_pressed("quit"):
 			get_tree().quit()
 
+#endregion
 
 func manage_limits() -> void:
 	player.position.y = clamp(player.position.y, level.top, level.bottom)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	hp_bar.bar_name = "HP"
+	hp_bar.update_value(100, 100)
+	player.PlayerHit.connect(hp_bar.update_value)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
