d46d664efd003362f25c409ffe8d33750e8ed8d1
Nick B <nbourrerg@hotmail.com>
2024-10-31 10:24:49 -0400

    wip : Mob

---
+++ wip/i'm-cool-and-i-fly/scenes/characters/generic_character.gd
@@ -0,0 +1,25 @@
+class_name Mob
+extends GenericCharacter
+
+signal PlayerHit(value : int, max_value : int)
+var hp : int = 100
+var max_hp : int = 100
+
+@export var bullet_scene : PackedScene = load("res://scenes/projectiles/bullet.tscn")
+@onready var hurt_box : Area2D = $Area2D
+
+func _ready() -> void:
+	anim_player = $AnimationPlayer
+	sprite = $Sprite2D
+	anim_player.play("fly")
+
+func _physics_process(delta: float) -> void:
+	if velocity.x > 0 :
+		sprite.flip_h = false
+	elif velocity.x < 0 :
+		sprite.flip_h = true
+		
+	move_and_slide()
+
+func is_touched() -> bool:
+	return false
